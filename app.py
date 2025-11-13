f6d0240d419593b016f301c634902f46bc77db73
Alastair McBride <110579154+amcbhome@users.noreply.github.com>
2025-11-13 05:47:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-import streamlit as st
-
-st.set_page_config(page_title="Lichess Study Viewer", layout="centered")
-
 st.title("Lichess Study Viewer")
-st.write("Embedded study:")
 
-# Embed the Lichess study
 st.components.v1.iframe(
     "https://lichess.org/study/embed/sgYuHjtP/jxQIk87O",
     width=600,
